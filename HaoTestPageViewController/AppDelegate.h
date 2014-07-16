cad771afc8969eaaf0cbb5c19780820dbc433592
BabyShung <hao-zheng@uiowa.edu>
2014-07-15 21:07:29 -0500

    Initial Commit

---
+++ HaoTestPageViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HaoTestPageViewController
+//
+//  Created by Hao Zheng on 7/15/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HaoTestPageViewController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HaoTestPageViewController
+//
+//  Created by Hao Zheng on 7/15/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
